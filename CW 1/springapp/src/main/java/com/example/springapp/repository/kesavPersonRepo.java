54cc8847cd539260283df6dfc427025aad05cf71
23cse504 <166788062+23cse504@users.noreply.github.com>
2024-04-24 14:26:32 +0530

    Add files via upload

---
+++ CW 1/springapp/src/main/java/com/example/springapp/repository/kesavPersonRepo.java
@@ -0,0 +1,14 @@
+package com.example.springapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.springapp.model.kesavPerson;
+import java.util.List;
+
+
+@Repository
+public interface kesavPersonRepo extends JpaRepository<kesavPerson,Integer>{
+
+    List<kesavPerson> findByAge(int age);
+    
+}
